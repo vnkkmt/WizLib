f38d6f4280a649c6b93f1bbc78a4b9559b6c30d7
Vinayak Kamath <vkamath@jdbyrider.com>
2022-05-23 16:17:20 +0530

    Data Annotations-3

--- WizLib_Models/Models/Book.cs
+++ WizLib_Models/Models/Book.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,8 +15,12 @@ namespace WizLib_Models.Models
         [Required]
         public string Title { get; set; }
         [Required]
+        [MaxLength(15)]
         public string ISBN { get; set; }
         [Required]
         public string Price { get; set; }
+
+        [NotMapped]
+        public string PriceRange { get; set; }
     }
 }
