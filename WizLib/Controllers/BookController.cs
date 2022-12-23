be290a76654e3994b74494caf86b4c6ea0abbce5
Vinayak Kamath <vkamath@jdbyrider.com>
2022-12-23 11:01:29 +0530

    Eager loading replacing explicit loading

--- WizLib/Controllers/BookController.cs
+++ WizLib/Controllers/BookController.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using WizLib_DataAccess.Data;
 using WizLib_Models.Models;
 using WizLib_Models.ViewModels;
+using Microsoft.EntityFrameworkCore;
 
 namespace WizLib.Controllers
 {
@@ -20,17 +21,66 @@ namespace WizLib.Controllers
         }
         public IActionResult Index()
         {
-            List<Book> objList = _db.Books.ToList();
-            foreach(var obj in objList)
-            {
+            //Eager loading better than explicit loading
+            List<Book> objList = _db.Books.Include(u => u.Publisher).ToList();
+
+            //foreach(var obj in objList)
+            //{
                 //least efficient
                 // obj.Publisher = _db.Publishers.FirstOrDefault(g => g.Publisher_Id == obj.Publisher_Id);
 
                 //Explicit loading more efficient
-                _db.Entry(obj).Reference(u => u.Publisher).Load();
-            }
+               // _db.Entry(obj).Reference(u => u.Publisher).Load();
+            //}
             return View(objList);
         }
+
+        public IActionResult Details(int? id)
+        {
+            BookVM obj = new BookVM();
+
+            if (id == null)
+            {
+                return View(obj);
+            }
+            //this for edit
+            //Eager loading
+            obj.Book = _db.Books.Include(u => u.BookDetail).FirstOrDefault();
+            //obj.Book = _db.Books.FirstOrDefault(u => u.Book_Id == id);
+            //obj.Book.BookDetail = _db.BookDetails.FirstOrDefault(i => i.BookDetail_Id == obj.Book.BookDetail_Id);
+            //if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Details(BookVM obj)
+        {
+            if (obj.Book.BookDetail.BookDetail_Id == 0)
+            {
+                //this is create
+                _db.BookDetails.Add(obj.Book.BookDetail);
+                _db.SaveChanges();
+
+                var BookFromDB = _db.Books.FirstOrDefault(i => i.BookDetail_Id == obj.Book.BookDetail_Id);
+                BookFromDB.BookDetail_Id = obj.Book.BookDetail.BookDetail_Id;
+                _db.SaveChanges();
+            }
+            else
+            {
+                //this is update
+                _db.BookDetails.Update(obj.Book.BookDetail); 
+                _db.SaveChanges();
+            }
+
+            return RedirectToAction(nameof(Index));
+
+        }
+
+
 
         public IActionResult Upsert(int? id)
         {
