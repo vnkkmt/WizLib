9a83f4e49081b1a6149aa1171cf810d85192ac75
Vinayak Kamath <vkamath@jdbyrider.com>
2022-12-23 17:40:16 +0530

    Adding View and SP for data GET

--- WizLib_DataAccess/Data/ApplicationDbContext.cs
+++ WizLib_DataAccess/Data/ApplicationDbContext.cs
@@ -15,7 +15,7 @@ namespace WizLib_DataAccess.Data
         {
 
         }
-
+        public DbSet<BookDetailsFromView> BookDetailsFromView { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Genre> Genres { get; set; }
         public  DbSet<Book> Books { get; set; }
@@ -47,6 +47,7 @@ namespace WizLib_DataAccess.Data
             modelBuilder.ApplyConfiguration(new FluentPublisherConfig());
             modelBuilder.ApplyConfiguration(new FluentAuthorConfig());
 
+            modelBuilder.Entity<BookDetailsFromView>().HasNoKey().ToView("GetOnlyBookDetails");
 
         }
 
